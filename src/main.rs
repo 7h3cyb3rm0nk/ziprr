4c448122bd27931fbb2c60c4af2263a76dbdc4aa
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-01-29 17:46:10 +0530

    add main function to call finder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+pub mod worker;
+pub mod reader;
+pub mod finder;
+use crate::finder::password_finder;
 fn main() {
-
+    let zip_path = env::args().nth(1).unwrap();
+    let dictionary_path = "/home/ramees/Downloads/puzzle/wooden_door_files/wordlist.txt";
+    let workers = 3;
+    password_finder(&zip_path, dictionary_path, workers);
 }
